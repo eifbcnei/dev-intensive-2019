0ed8c7add98f2e5c7d39c94b7ec5c76282592727
eifbcnei <maxim99nn@gmail.com>
2019-07-02 11:10:23 +0300

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
@@ -12,6 +12,6 @@ class ImageMessage(
     var image: String? = null
 ) :
     BaseMessage(id, from, chat, isIncoming, date) {
-   override fun formatMessage() :String="${from?.firstName} ${if(isIncoming) "получил" else "отправил"} изображение \"text\" ${date.humanizeDiff()}"
+   override fun formatMessage() :String="${from?.firstName} ${if(isIncoming) "получил" else "отправил"} изображение \"$image\" ${date.humanizeDiff()}"
 
 }
--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -13,5 +13,5 @@ class TextMessage(
     var text: String?
 ) :
 BaseMessage(id, from, chat, isIncoming, date) {
-    override fun formatMessage() :String="${from?.firstName} ${if(isIncoming) "получил" else "отправил"} сообщение \"text\" ${date.humanizeDiff()}"
+    override fun formatMessage() :String="${from?.firstName} ${if(isIncoming) "получил" else "отправил"} сообщение \"$text\" ${date.humanizeDiff()}"
 }
