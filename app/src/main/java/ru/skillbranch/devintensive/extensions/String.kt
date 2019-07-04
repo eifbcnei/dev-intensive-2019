4da1ec6447d2a7998b5a1b5e3c73bf82eb91a711
eifbcnei <maxim99nn@gmail.com>
2019-07-04 17:14:51 +0300

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,5 +1,5 @@
 package ru.skillbranch.devintensive.extensions
-
+/*
 fun String.truncate(length: Int = 16): String {
     trim()
     var result = StringBuilder()
@@ -14,3 +14,14 @@ fun String.truncate(length: Int = 16): String {
     if (result.length >= length - 1) result.append("...")
     return result.toString()
 }
+*/
+fun String.truncate(len: Int = 16): String {
+    val result = this.trim()
+    return if (result.length <= len + 1) result else "${result.take(len + 1).trimEnd()}..."
+}
+
+fun String.stripHtml(): String =
+    this
+        .replace("""<.*?>""".toRegex(), "")
+        .replace("""&(#\d+?|\w+?);""".toRegex(), "")
+        .split(""" +""".toRegex()).joinToString(" ")
