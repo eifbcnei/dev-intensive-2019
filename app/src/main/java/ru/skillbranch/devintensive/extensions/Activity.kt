589d49d1f112ecbb4fcea14f924b009be08b9fc8
eifbcnei <maxim99nn@gmail.com>
2019-07-14 17:45:25 +0300

    hometask_3 complete

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -0,0 +1,38 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Rect
+import android.util.TypedValue
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlin.math.roundToLong
+
+fun Activity.hideKeyboard() {
+    var focusedView: View = this.currentFocus
+    var imm: InputMethodManager = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(focusedView.windowToken, 0)
+}
+
+fun Activity.isKeyboardOpen(): Boolean {
+    fun Context.convertDpToPx(dp: Float): Float {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            this.resources.displayMetrics
+        )
+    }
+
+    val rootView = findViewById<View>(android.R.id.content)
+    val visibleBounds = Rect()
+    rootView.getWindowVisibleDisplayFrame(visibleBounds)
+    val heightDiff = rootView.height - visibleBounds.height()
+    val marginOfError = this.convertDpToPx(50F).roundToLong()
+
+    return heightDiff > marginOfError
+}
+
+fun Activity.isKeyboardClosed(): Boolean {
+    return this.isKeyboardOpen().not()
+}
+
---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -0,0 +1,82 @@
+package ru.skillbranch.devintensive.models
+
+class Bender(var status: Status = Status.NORMAL, var question: Question = Question.NAME) {
+    enum class Status(val color: Triple<Int, Int, Int>) {
+        NORMAL(Triple(255, 255, 255)),
+        WARNING(Triple(255, 120, 0)),
+        DANGER(Triple(255, 60, 60)),
+        CRITICAL(Triple(255, 0, 0));
+
+        fun nextStatus(): Status {
+            return if (this.ordinal < values().lastIndex) {
+                values()[this.ordinal + 1]
+            } else {
+                values()[0]
+            }
+        }
+
+    }
+
+    fun askQuestion() = question.question
+
+    fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
+        val (isValid, comment) = question.testInput(answer)
+        return if (isValid) {
+            if (question.answers.contains(answer)) {
+                question = question.nextQuestion()
+                "Отлично - ты справился!\n${question.question}" to status.color
+            } else {
+                status = status.nextStatus()
+                // 3 попытки, иначе - заново
+                if (status == Status.NORMAL) {
+                    question = Question.NAME
+                }
+                "Это неправильный ответ!\n${question.question}" to status.color
+            }
+        } else {
+            "$comment\n${question.question}" to status.color
+        }
+    }
+
+
+    enum class Question(val question: String, val answers: List<String>) {
+        NAME("Как меня зовут?", listOf("Бендер", "bender")) {
+            override fun testInput(answer: String): Pair<Boolean, String> =
+                answer[0].isUpperCase() to "Имя должно начинаться с заглавной буквы"
+
+            override fun nextQuestion(): Question = PROFESSION
+        },
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
+            override fun testInput(answer: String): Pair<Boolean, String> =
+                answer[0].isLowerCase() to "Профессия должна начинаться со строчной буквы"
+
+            override fun nextQuestion(): Question = MATERIAL
+        },
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
+            override fun testInput(answer: String): Pair<Boolean, String> =
+                answer.any { !it.isDigit() } to "Материал не должен содержать цифр"
+
+            override fun nextQuestion(): Question = BDAY
+        },
+        BDAY("Когда меня создали?", listOf("2993")) {
+            override fun testInput(answer: String): Pair<Boolean, String> =
+                answer.any { it.isDigit() } to "Год моего рождения должен содержать только цифры"
+
+            override fun nextQuestion(): Question = SERIAL
+        },
+        SERIAL("Мой серийный номер?", listOf("2716057")) {
+            override fun testInput(answer: String): Pair<Boolean, String> =
+                (answer.length == 7 && answer.any { it.isDigit() }) to "Серийный номер содержит только цифры, и их 7"
+
+            override fun nextQuestion(): Question = IDLE
+        },
+        IDLE("На этом все, вопросов больше нет", listOf()) {
+            override fun testInput(answer: String): Pair<Boolean, String> = true to ""//игнорируем валидацию
+
+            override fun nextQuestion(): Question = IDLE
+        };
+
+        abstract fun nextQuestion(): Question
+        abstract fun testInput(answer: String): Pair<Boolean, String>
+    }
+}
